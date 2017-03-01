8f94dfdb6289c2859d136c24c82651da3705f2d4
Al Aganya <tbone.aa87@gmail.com>
2017-03-01 18:26:35 +0300

    Edit 2.0.0

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+$(document).ready(function(){
+	$(".firstdisplay").fadeIn(4000);
+});
